da914cac9045f3ddbd01b3da222f7ccffc5cff70
Ricard Mallafre <rikitzzz@gmail.com>
2023-03-19 22:23:25 +0100

    chore: readability improvements

--- src/Version.ts
+++ src/Version.ts
@@ -11,9 +11,11 @@ export class Version {
   constructor(semVer: string) {
     logger.debug({ semVer }, 'new Version instance');
     const [major, minor, patch] = semVer.split('.').map(n => parseInt(n, 10));
+
     if (typeof major !== 'number') throw new Error(`Could not figure out major in ${semVer}`);
     if (typeof minor !== 'number') throw new Error(`Could not figure out minor in ${semVer}`);
     if (typeof patch !== 'number') throw new Error(`Could not figure out patch in ${semVer}`);
+
     [this.major, this.minor, this.patch] = [major, minor, patch];
     this._raw = semVer;
   }
@@ -24,9 +26,11 @@ export class Version {
 
   isNewerThan(version: Version): boolean {
     logger.debug({ this: this, other: version }, 'comparing if this is newer than other');
+
     if (this.major !== version.major) return this.major > version.major;
     if (this.minor !== version.minor) return this.minor > version.minor;
     if (this.patch !== version.patch) return this.patch > version.patch;
+
     return false;
   }
 
